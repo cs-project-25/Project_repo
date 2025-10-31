5f73cccf8f238a84a44c04ab356ac9b281d2dabb
miabiedermann <miabiedermann@icloud.com>
2025-10-31 09:54:21 +0100

    tried something

--- app.py
+++ app.py
@@ -1,4 +1,9 @@
+import sys
 import streamlit as st
+
+st.write("Python executable Streamlit is using:")
+st.code(sys.executable)
+
 st.title("This is our app")
 st.write("We connected everything")
 st.write("I'm losing my nerves")
